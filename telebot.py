b0f188b3f75b7bbf7d655161fe84d1eafcc430e3
koshkinaav <91763632+koshkinaav@users.noreply.github.com>
2022-09-13 18:57:06 +0300

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -4,20 +4,24 @@ from telebot import types
 import pandas as pd
 import numpy as np
 import time
+import os
+
+lamour = ['Маменька красавица!', 'Совсем меня разлюбила(', 'Маменька у меня принцесса', 'Пора отдыхать',
+          'Люблю, люблю маменьку!']
+lamour = np.array(lamour)
 
 def Sv(file1, file2):
-    df_1C = pd.read_csv(file1, sep='    ', dtype='string', names=['code', 'quantity'])
-    df_Sveta = pd.read_csv(file2, sep='    ', dtype='string', names=['code', 'quantity'])
+    df_1C = pd.read_excel(file1, dtype='string', names=['code', 'quantity'])
+    df_Sveta = pd.read_excel(file2,dtype='string', names=['code', 'quantity'])
     df_1C['quantity'] = df_1C['quantity'].str.replace(',', '.')
     df_Sveta['quantity'] = df_Sveta['quantity'].str.replace(',', '.')
-    df_1C['quantity'] = df_1C['quantity'].str.replace(' ', '')
-    df_Sveta['quantity'] = df_Sveta['quantity'].str.replace(' ', '')
+    df_1C['quantity'] = df_1C['quantity'].str.replace(' ', '')
+    df_Sveta['quantity'] = df_Sveta['quantity'].str.replace(' ', '')
     df_1C['quantity'] = df_1C['quantity'].astype('float')
     df_Sveta['quantity'] = df_Sveta['quantity'].astype('float')
     df = df_1C.merge(df_Sveta, on='code', how='outer')
     x = df[df['quantity_x'] != df['quantity_y']]
     x.to_excel('Сличительная.xlsx', index=False)
-
 
 bot = telebot.TeleBot('5407511525:AAG5Z9x6beOi5c49aeuH9DEspjdrPmQcEhw')
 
@@ -37,14 +41,15 @@ def start(m, res=False):
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     if message.text.strip() == 'Любовь':
-        answer = 'Я люблю тебя, мамуля!'
+        answer = np.random.choice(lamour)
     if message.text.strip() == 'Сводная таблица':
-        answer = 'Сбрось сначала файл Светы, потом свой (Назови первый "1.txt", второй "2.txt")'
+        answer = 'Сбрось сначала файл Светы, потом свой (Назови первый "1.xlsx", второй "2.xlsx")'
     if message.text.strip() == 'Начать работу':
-        Sv('1.txt', '2.txt')
+        Sv('1.xlsx', '2.xlsx')
         time.sleep(10)
         doc = open('Сличительная.xlsx', 'rb')
         bot.send_document(message.chat.id, doc)
+        os.remove('/Users/akonkina/pythonProject/Сличительная.xlsx')
         answer = 'Готово, целую!'
 
     bot.send_message(message.chat.id, answer)
@@ -66,7 +71,6 @@ def handle_docs_photo(message):
         bot.send_message(message.chat.id, 'Когда отправишь два файла, нажми "Начать работу"')
     except Exception as e:
         bot.reply_to(message, e)
-
 
 
 bot.polling(none_stop=True, interval=0)
